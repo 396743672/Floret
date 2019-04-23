9934084c4ba986d819af6ab92f6a2260de9e0a9e
zhongjiabao <957250254@qq.com>
2019-04-23 11:17:44 +0800

    初始化项目

---
+++ sql/floret.sql
@@ -0,0 +1,85 @@
+CREATE TABLE `floret_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `tenant_code` varchar(12) DEFAULT '000000' COMMENT '租户编号',
+  `account` varchar(45) DEFAULT NULL COMMENT '账号',
+  `password` varchar(45) DEFAULT NULL COMMENT '密码',
+  `name` varchar(20) DEFAULT NULL COMMENT '昵称',
+  `real_name` varchar(10) DEFAULT NULL COMMENT '真名',
+  `email` varchar(45) DEFAULT NULL COMMENT '邮箱',
+  `phone` varchar(45) DEFAULT NULL COMMENT '手机',
+  `birthday` datetime DEFAULT NULL COMMENT '生日',
+  `sex` smallint(6) DEFAULT NULL COMMENT '性别',
+  `role_id` varchar(255) DEFAULT NULL COMMENT '角色id',
+  `dept_id` varchar(255) DEFAULT NULL COMMENT '部门id',
+  `create_user` int(11) DEFAULT NULL COMMENT '创建人',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_user` int(11) DEFAULT NULL COMMENT '修改人',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `status` int(2) DEFAULT NULL COMMENT '状态',
+  `is_deleted` int(2) DEFAULT '0' COMMENT '是否已删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `floret_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `tenant_code` varchar(12) DEFAULT '000000' COMMENT '租户编号',
+  `parent_id` int(11) DEFAULT '0' COMMENT '父主键',
+  `role_name` varchar(255) DEFAULT NULL COMMENT '角色名',
+  `sort` int(11) DEFAULT NULL COMMENT '排序',
+  `role_alias` varchar(255) DEFAULT NULL COMMENT '角色别名',
+  `is_deleted` int(2) DEFAULT '0' COMMENT '是否已删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `floret_role_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `menu_id` int(11) DEFAULT NULL COMMENT '菜单id',
+  `role_id` int(11) DEFAULT NULL COMMENT '角色id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `floret_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `parent_id` int(11) DEFAULT '0' COMMENT '父级菜单',
+  `code` varchar(255) DEFAULT NULL COMMENT '菜单编号',
+  `name` varchar(255) DEFAULT NULL COMMENT '菜单名称',
+  `alias` varchar(255) DEFAULT NULL COMMENT '菜单别名',
+  `path` varchar(255) DEFAULT NULL COMMENT '请求地址',
+  `source` varchar(255) DEFAULT NULL COMMENT '菜单资源',
+  `sort` int(2) DEFAULT NULL COMMENT '排序',
+  `category` int(2) DEFAULT NULL COMMENT '菜单类型',
+  `action` int(2) DEFAULT '0' COMMENT '操作按钮类型',
+  `is_open` int(2) DEFAULT '1' COMMENT '是否打开新页面',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  `is_deleted` int(2) DEFAULT '0' COMMENT '是否已删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `floret_dept` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `tenant_code` varchar(12) DEFAULT '000000' COMMENT '租户编号',
+  `parent_id` int(11) DEFAULT '0' COMMENT '父主键',
+  `dept_name` varchar(45) DEFAULT NULL COMMENT '部门名',
+  `full_name` varchar(45) DEFAULT NULL COMMENT '部门全称',
+  `sort` int(11) DEFAULT NULL COMMENT '排序',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  `is_deleted` int(2) DEFAULT '0' COMMENT '是否已删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `floret_dict` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `parent_id` int(11) DEFAULT '0' COMMENT '父主键',
+  `code` varchar(255) DEFAULT NULL COMMENT '字典码',
+  `dict_key` int(2) DEFAULT NULL COMMENT '字典值',
+  `dict_value` varchar(255) DEFAULT NULL COMMENT '字典名称',
+  `sort` int(11) DEFAULT NULL COMMENT '排序',
+  `remark` varchar(255) DEFAULT NULL COMMENT '字典备注',
+  `is_deleted` int(2) DEFAULT '0' COMMENT '是否已删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
